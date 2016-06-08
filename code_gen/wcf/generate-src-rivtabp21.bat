7b417df9241605ea622ff81f5e7c53a537da7283
Albin Sunnanbo <albin@sunnanbo.com>
2016-06-08 16:17:41 +0200

    #SLLBESUTV-3547 - Lägg till organisationsträd i beställningen för tolkar
     - New Service GetUnitInformation

--- code_gen/wcf/generate-src-rivtabp21.bat
+++ code_gen/wcf/generate-src-rivtabp21.bat
@@ -1,29 +1,29 @@
 
-	@REM ---------------------------------------------------------------------------------
-	@REM Generates c# WCF service contracts (interface), client proxies and wcf config
-	@REM file for the WSDLs + XML Schemas of the service domain, using .Net WCF tool svcuti.exe
-	@REM ---------------------------------------------------------------------------------
-	@REM Licensed to the Apache Software Foundation (ASF) under one
-	@REM or more contributor license agreements. See the NOTICE file
-	@REM distributed with this work for additional information
-	@REM regarding copyright ownership. Inera AB licenses this file
-	@REM to you under the Apache License, Version 2.0 (the
-	@REM "License"); you may not use this file except in compliance
-	@REM with the License. You may obtain a copy of the License at
-	@REM
-	@REM http://www.apache.org/licenses/LICENSE-2.0
-	@REM Unless required by applicable law or agreed to in writing,
-	@REM software distributed under the License is distributed on an
-	@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-	@REM KIND, either express or implied. See the License for the
-	@REM specific language governing permissions and limitations
-	@REM under the License.
-	@REM ---------------------------------------------------------------------------------
-	CD ..\..
-	
-	SET SCHEMADIR=schemas
-	
-	SET W0=%SCHEMADIR%\interactions\AnswerInquiryInteraction\AnswerInquiryInteraction_1.0_RIVTABP21.wsdl
+@REM ---------------------------------------------------------------------------------
+@REM Generates c# WCF service contracts (interface), client proxies and wcf config
+@REM file for the WSDLs + XML Schemas of the service domain, using .Net WCF tool svcuti.exe
+@REM ---------------------------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements. See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership. Inera AB licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License. You may obtain a copy of the License at
+@REM
+@REM http://www.apache.org/licenses/LICENSE-2.0
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied. See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM ---------------------------------------------------------------------------------
+CD ..\..
+
+SET SCHEMADIR=schemas
+
+SET W0=%SCHEMADIR%\interactions\AnswerInquiryInteraction\AnswerInquiryInteraction_1.0_RIVTABP21.wsdl
 SET X0=%SCHEMADIR%\interactions\AnswerInquiryInteraction\*.xsd
 
 SET W1=%SCHEMADIR%\interactions\ListBookingsInteraction\ListBookingsInteraction_1.0_RIVTABP21.wsdl
@@ -38,9 +38,12 @@ SET X3=%SCHEMADIR%\interactions\ListLanguagesInteraction\*.xsd
 SET W4=%SCHEMADIR%\interactions\UpdateBookingInteraction\UpdateBookingInteraction_1.0_RIVTABP21.wsdl
 SET X4=%SCHEMADIR%\interactions\UpdateBookingInteraction\*.xsd
 
+SET W5=%SCHEMADIR%\interactions\GetUnitInformationInteraction\GetUnitInformationInteraction_1.0_RIVTABP21.wsdl
+SET X5=%SCHEMADIR%\interactions\GetUnitInformationInteraction\*.xsd
+
 SET XCORE=%SCHEMADIR%\core_components\*.xsd
 
-SET SCHEMAS=%XCORE% %W0% %X0% %W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% 
+SET SCHEMAS=%XCORE% %W0% %X0% %W1% %X1% %W2% %X2% %W3% %X3% %W4% %X4% %W5% %X5% 
 
 SET OUTFILE=code_gen\wcf\generated-src\SupportprocessPersonalresourcesInterpretationInteractions.cs
 SET PRAGMAFILE=code_gen\wcf\generated-src\pragma.cs
