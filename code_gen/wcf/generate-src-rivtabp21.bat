c406e0395d7297a64b5d9da6c7134102e127d571
Albin Sunnanbo <albin@sunnanbo.com>
2016-06-14 07:17:01 +0200

    Update source generators

--- code_gen/wcf/generate-src-rivtabp21.bat
+++ code_gen/wcf/generate-src-rivtabp21.bat
@@ -1,45 +1,45 @@
 
-@REM ---------------------------------------------------------------------------------
-@REM Generates c# WCF service contracts (interface), client proxies and wcf config
-@REM file for the WSDLs + XML Schemas of the service domain, using .Net WCF tool svcuti.exe
-@REM ---------------------------------------------------------------------------------
-@REM Licensed to the Apache Software Foundation (ASF) under one
-@REM or more contributor license agreements. See the NOTICE file
-@REM distributed with this work for additional information
-@REM regarding copyright ownership. Inera AB licenses this file
-@REM to you under the Apache License, Version 2.0 (the
-@REM "License"); you may not use this file except in compliance
-@REM with the License. You may obtain a copy of the License at
-@REM
-@REM http://www.apache.org/licenses/LICENSE-2.0
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied. See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
-@REM ---------------------------------------------------------------------------------
-CD ..\..
-
-SET SCHEMADIR=schemas
-
-SET W0=%SCHEMADIR%\interactions\AnswerInquiryInteraction\AnswerInquiryInteraction_1.0_RIVTABP21.wsdl
+	@REM ---------------------------------------------------------------------------------
+	@REM Generates c# WCF service contracts (interface), client proxies and wcf config
+	@REM file for the WSDLs + XML Schemas of the service domain, using .Net WCF tool svcuti.exe
+	@REM ---------------------------------------------------------------------------------
+	@REM Licensed to the Apache Software Foundation (ASF) under one
+	@REM or more contributor license agreements. See the NOTICE file
+	@REM distributed with this work for additional information
+	@REM regarding copyright ownership. Inera AB licenses this file
+	@REM to you under the Apache License, Version 2.0 (the
+	@REM "License"); you may not use this file except in compliance
+	@REM with the License. You may obtain a copy of the License at
+	@REM
+	@REM http://www.apache.org/licenses/LICENSE-2.0
+	@REM Unless required by applicable law or agreed to in writing,
+	@REM software distributed under the License is distributed on an
+	@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+	@REM KIND, either express or implied. See the License for the
+	@REM specific language governing permissions and limitations
+	@REM under the License.
+	@REM ---------------------------------------------------------------------------------
+	CD ..\..
+	
+	SET SCHEMADIR=schemas
+	
+	SET W0=%SCHEMADIR%\interactions\AnswerInquiryInteraction\AnswerInquiryInteraction_1.0_RIVTABP21.wsdl
 SET X0=%SCHEMADIR%\interactions\AnswerInquiryInteraction\*.xsd
 
-SET W1=%SCHEMADIR%\interactions\ListBookingsInteraction\ListBookingsInteraction_1.0_RIVTABP21.wsdl
-SET X1=%SCHEMADIR%\interactions\ListBookingsInteraction\*.xsd
+SET W1=%SCHEMADIR%\interactions\GetUnitInformationInteraction\GetUnitInformationInteraction_1.0_RIVTABP21.wsdl
+SET X1=%SCHEMADIR%\interactions\GetUnitInformationInteraction\*.xsd
 
-SET W2=%SCHEMADIR%\interactions\ListInquiriesInteraction\ListInquiriesInteraction_1.0_RIVTABP21.wsdl
-SET X2=%SCHEMADIR%\interactions\ListInquiriesInteraction\*.xsd
+SET W2=%SCHEMADIR%\interactions\ListBookingsInteraction\ListBookingsInteraction_1.0_RIVTABP21.wsdl
+SET X2=%SCHEMADIR%\interactions\ListBookingsInteraction\*.xsd
 
-SET W3=%SCHEMADIR%\interactions\ListLanguagesInteraction\ListLanguagesInteraction_1.0_RIVTABP21.wsdl
-SET X3=%SCHEMADIR%\interactions\ListLanguagesInteraction\*.xsd
+SET W3=%SCHEMADIR%\interactions\ListInquiriesInteraction\ListInquiriesInteraction_1.0_RIVTABP21.wsdl
+SET X3=%SCHEMADIR%\interactions\ListInquiriesInteraction\*.xsd
 
-SET W4=%SCHEMADIR%\interactions\UpdateBookingInteraction\UpdateBookingInteraction_1.0_RIVTABP21.wsdl
-SET X4=%SCHEMADIR%\interactions\UpdateBookingInteraction\*.xsd
+SET W4=%SCHEMADIR%\interactions\ListLanguagesInteraction\ListLanguagesInteraction_1.0_RIVTABP21.wsdl
+SET X4=%SCHEMADIR%\interactions\ListLanguagesInteraction\*.xsd
 
-SET W5=%SCHEMADIR%\interactions\GetUnitInformationInteraction\GetUnitInformationInteraction_1.0_RIVTABP21.wsdl
-SET X5=%SCHEMADIR%\interactions\GetUnitInformationInteraction\*.xsd
+SET W5=%SCHEMADIR%\interactions\UpdateBookingInteraction\UpdateBookingInteraction_1.0_RIVTABP21.wsdl
+SET X5=%SCHEMADIR%\interactions\UpdateBookingInteraction\*.xsd
 
 SET XCORE=%SCHEMADIR%\core_components\*.xsd
 
